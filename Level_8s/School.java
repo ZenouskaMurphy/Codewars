128a97fe4ca21dc1495f7ba0e489b265c4ef439c
ZenouskaMurphy <zenouska_murphy@hotmail.com>
2021-02-09 22:30:21 +0000

    Zen Update 09/02/2021

--- Level_8s/School.java
+++ Level_8s/School.java
@@ -1,3 +1,5 @@
+package Level_8s;
+
 /* 
 It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
 
