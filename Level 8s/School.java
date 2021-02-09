64033fbffd5955115f2d1b4f8067889c04a1e144
ZenouskaMurphy <zenouska_murphy@hotmail.com>
2021-02-09 19:09:44 +0000

    Zen Update 09/02/2021

---
+++ Level 8s/School.java
@@ -0,0 +1,28 @@
+/* 
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/ 
+
+/* Solution 1 */
+public class School{
+    public static int getAverage(int[] marks){
+      int sum = 0;
+      for(int i = 0; i < marks.length; i++ ){
+        sum = sum + marks[i];
+      }
+      return sum / marks.length;
+    }
+
+/*Solution 2 */
+    public static int getAverage2(int[] marks){
+      int sum = 0;
+      for (int mark : marks) {
+        sum += mark;
+      } 
+      return sum / marks.length;
+    }
+  }
+  
