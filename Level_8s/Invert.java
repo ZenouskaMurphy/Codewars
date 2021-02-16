428a0d4c98d08c35df08d33573be33a1cc259ca1
ZenouskaMurphy <zenouska_murphy@hotmail.com>
2021-02-16 17:12:52 +0000

    Zen Update 16/02/2021

--- Level_8s/Invert.java
+++ Level_8s/Invert.java
@@ -1,4 +1,4 @@
-/* NOT COMPLETED
+/*COMPLETED
 Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
 invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
